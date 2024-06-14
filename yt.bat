85123fc71586ccf2c69f84e1e90941b09bb9fdae
Satyar <stking68@yahoo.com>
2024-06-14 12:55:06 +0330

    Batch Script Added

---
+++ yt.bat
@@ -0,0 +1,8 @@
+@ECHO OFF
+
+@REM Add Your Location down blow!
+start javaw \Users\sat\Desktop\YTSearcher_V1.0.java 
+
+@REM Or in case of a Jar File Use
+@REM start javaw -jar your_jar_file_name.jar
+
